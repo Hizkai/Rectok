702da224663daa7bcd98b3b163b13bbc43ee9515
zhangkai <zhangkai@appledeMacBook-Pro.local>
2025-05-22 09:51:33 +0800

    update

--- shell/get_instruments_token.sh
+++ shell/get_instruments_token.sh
@@ -1,12 +1,13 @@
 python ../tokenizer/main.py \
     --data_name Instruments \
+    --normalize \
     --gpu_id 0 \
     --epochs 100 \
     --patience 200 \
     --batch_size 1024 \
     --lr 1e-4 \
+    --L 5 \
     --alpha 0.001 \
     --num_codes 1024 \
-    --tau 0.25 \
-    --L 5 \
-    --normalize \
+    --tau 0.25
+    
